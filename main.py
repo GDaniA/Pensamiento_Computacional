03aef14a567529f352352d3573aa95c291b2daad
Daniel <ag.garcia.daniel@gmail.com>
2024-05-08 08:56:00 -0600

    Creacion de main.py
    Creacion de main.py con datos del estudiante

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-
+print("Daniel Alejandro García García")
+print("Carné: 1665924")
+print("18 años, proximamente 19")
+print("Residencia: Quetzaltenango")
+print("Curso: Introduccion a la ingenieria en informatica y sistemas")
